2ded2e1fc9c27f44d9b21cbb102a7cd43bd73d07
pratyushvox <np03cs4s230182@heraldcollege.edu.np>
2024-03-18 13:09:18 +0545

    Add register form

---
+++ Frontend/js/script.js
@@ -0,0 +1,69 @@
+// const items=[1,29,47];
+// const copy=[];
+// items.forEach(functions(items){
+//     copy.push(items*items);
+
+// });
+// console.log(copy);
+// var abc={
+//     "name":"raj",
+//     "age":19,
+//     "section":"A",
+//      Total: function() {
+//        console.log("This is inside object")
+//     }
+//   }
+//   console.log(abc.Total)
+
+
+// javascript object
+// var jsobject={
+//     name:"raj",
+//     age:19,
+//     section:"A",
+//      Total: function() {
+//        console.log("Hello wolrd")
+//     }
+//   }
+//  var a=JSON.stringify(jsobject) // this is how we covert javascript object in to jason object
+//  console.log(a)
+
+
+// converting jason object in to javascript object
+
+// var jsonobj= '{"name":"Ram","age":21,"Address":"Pokhara"}'
+// var val=JSON.parse(jsonobj)
+// console.log(val)
+// "strict mode"// prevents accidental error
+//prevent us from doing certain things
+"use strict"
+// let hasdriverslicense= false;
+// const passtest = true;
+// if(passtest){
+//     hasdriverslicense = true;
+// }
+// if(hasdriverslicense){
+//     console.log("I am a driver")
+
+
+// }
+// let array1=[1,2,3,5]
+// array1.push(4)
+// console.log(array1)
+// array1.pop()
+// console.log(array1)
+// array1.unshift(12)
+// console.log(array1)
+// array1.shift()
+// console.log(array1)
+// let 
+// a=array1.indexOf(5)
+// console.log(array1)
+// console.log(a)
+// console.log(array1.includes("C"))
+
+let firstname="pratyush"
+let lastname="khadka"
+console.log("my name is " + firstname + " and my last name is " + lastname )
+console.log(`my name is ${firstname} and my last name is $
+{lastname}`)
