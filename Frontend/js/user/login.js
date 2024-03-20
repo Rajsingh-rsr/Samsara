f38146a6b12b31dcb6689322f46052baeba2d5cd
aryanchakradhar <aryanchakradhar11@gmail.com>
2024-03-20 21:47:40 +0545

    Add Login page

---
+++ Frontend/js/user/login.js
@@ -0,0 +1,63 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Validate email
+    var email = document.getElementById('email').value;
+    var emailError = document.getElementById('emailError');
+    if (!validateEmail(email)) {
+        emailError.innerText = 'Invalid email format';
+        return;
+    } else {
+        emailError.innerText = '';
+    }
+
+    // Validate password (optional)
+    var password = document.getElementById('password').value;
+    var passwordError = document.getElementById('passwordError');
+    if (password.length < 8) {
+        passwordError.innerText = 'Password must be at least 8 characters';
+        return;
+    } else {
+      passwordError.innerText = '';
+    }
+
+    // Prepare data for POST request
+    var formData = {
+        email: email,
+        password: password,
+    };
+    console.log(formData);
+
+    fetch('https://newtest-production-f50e.up.railway.app/api/v1/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+     })
+
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle success response
+        console.log('Form submitted successfully', data);
+    })
+    .catch(error => {
+        // Handle error
+        console.error('There was a problem with the form submission:', error);
+    });
+})
+// Function to validate email format
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+
+
+
+
