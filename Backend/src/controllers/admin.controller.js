63df7cb1e41fc184ae7bedf9d589e38abbf17131
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-05-14 00:47:38 +0545

    Add user visited log endpoint to admin

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -4,7 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import mongoose, { isValidObjectId } from "mongoose"
-
+import fs from "fs"
 import jwt from "jsonwebtoken"
 
 
@@ -69,8 +69,42 @@ const AllSeller = asyncHandler(async (req, res) => {
 
 })
 
+const userVisited = asyncHandler(async (req, res) => {
+
+    fs.readFile("./src/log/totalVisits.txt", "utf-8", (err, data) => {
+        if (err) {
+            console.log("Error ", err);
+            throw new ApiError(500, "something went wrong while fetching data ")
+
+        }
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { totalVisitd: data }, "user visited fetched sucessfully"))
+
+    })
+})
+
+const userVisitedLog = asyncHandler(async (req, res) => {
+
+    fs.readFile("./src/log/log.txt", "utf-8", (err, data) => {
+        if (err) {
+            console.log("Error ", err);
+            throw new ApiError(500, err)
+        }
+
+        const logArray = data.trim().split('\n');
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, logArray, "user visited fetched sucessfully"))
+
+    })
+})
 
 export {
     removeSeller,
-    AllSeller
+    AllSeller,
+    userVisited,
+    userVisitedLog
 }
--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 
 import {
     removeSeller,
-    AllSeller
+    AllSeller,
+    userVisited,
+    userVisitedLog
 } from "../controllers/admin.controller.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -15,6 +17,8 @@ router.use(verifyJWT)
 
 router.route("/remove/user/:userId").delete(removeSeller)
 router.route("/allseller").get(AllSeller)
+router.route("/uservisited").get(userVisited)
+router.route("/uservisitedLog").get(userVisitedLog)
 
 
 export default router
