76557b746388aa4fa602f418407d5e7ca71a77f8
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-05-14 01:01:39 +0545

    Add get all customer endpoint to admin

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -4,7 +4,8 @@ import {
     removeSeller,
     AllSeller,
     userVisited,
-    userVisitedLog
+    userVisitedLog,
+    allCustomer
 } from "../controllers/admin.controller.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -19,6 +20,7 @@ router.route("/remove/user/:userId").delete(removeSeller)
 router.route("/allseller").get(AllSeller)
 router.route("/uservisited").get(userVisited)
 router.route("/uservisitedLog").get(userVisitedLog)
+router.route("/allcustomer").get(allCustomer)
 
 
 export default router
